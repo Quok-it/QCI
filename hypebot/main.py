bdc28ecf5a39b4b67021e2632e802c42708c1d40
aabdel0181 <ahmedabdellahhoco@gmail.com>
2025-04-25 16:56:15 -0400

    add: only find H100s

--- hypebot/main.py
+++ hypebot/main.py
@@ -8,7 +8,7 @@ def main():
     logger.log("Starting QuokBot...")
 
     # Get available GPUs (array)
-    available_gpus = marketplace_client.list_available_gpus()
+    available_gpus = marketplace_client.list_available_gpus(gpu_name_filter="H100")
 
     for gpu in available_gpus:
         print(f"Available GPU: {gpu['gpu_model']} for ${gpu['price_per_hour']}/hr at {gpu['region']}")
--- hypebot/marketplace_client.py
+++ hypebot/marketplace_client.py
@@ -4,7 +4,8 @@ class MarketplaceClient:
     def __init__(self):
         self.marketplace_url = "https://api.hyperbolic.xyz/v1/marketplace"
 
-    def list_available_gpus(self, filters: dict = None) -> list:
+    # Lists available GPUs with optional API filters and optional name 
+    def list_available_gpus(self, filters: dict = None, gpu_name_filter: str = None) -> list:
         payload = {"filters": filters or {}}
         headers = {
             "Content-Type": "application/json"
@@ -22,13 +23,21 @@ class MarketplaceClient:
                 not instance["reserved"] and
                 instance["gpus_reserved"] < instance["gpus_total"]):
                 
-                available_instances.append({
-                    "node_id": instance["id"],
-                    "cluster_name": instance["cluster_name"],
-                    "gpu_model": instance["hardware"]["gpus"][0]["model"] if instance["hardware"]["gpus"] else None,
-                    "gpu_ram": instance["hardware"]["gpus"][0]["ram"] if instance["hardware"]["gpus"] else None,
-                    "price_per_hour": instance["pricing"]["price"]["amount"]/100,
-                    "region": instance["location"]["region"]
-                })
+                # get the GPU model 
+                gpu_model = instance["hardware"]["gpus"][0]["model"]if instance["hardware"]["gpus"] else None
+
+                # Only return specified GPU
+                if gpu_model: 
+                    # checks if we have no filter or if we match a given filter 
+                    if gpu_name_filter is None or gpu_name_filter in gpu_model:
+                        available_instances.append({
+                            "node_id": instance["id"],
+                            "cluster_name": instance["cluster_name"],
+                            "gpu_model": gpu_model,
+                            "gpu_ram": instance["hardware"]["gpus"][0]["ram"] if instance["hardware"]["gpus"] else None,
+                            "price_per_hour": instance["pricing"]["price"]["amount"]/100,
+                            "region": instance["location"]["region"]
+                        })
+
 
         return available_instances
